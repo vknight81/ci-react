6617b1720125b978dc5e254e05af18d53472fe81
Vince Knight <vknight81@gmail.com>
2017-10-24 23:56:50 -0400

    working environment complete

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "C:\\Users\\Vince Knight\\ci-react\\pages\\index.js?entry";
+
+//used Next js to get setup -- final command npm run dev to get server started
+//in Next js anything placed within your pages folder will be a route within your application.  this folder must be named "pages".  All other folders may be name something else.
+//the routes are automatically built for you when you create the page names!  e.g. about.js will route to localhost:3000/about
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, "Welcome to next.js!");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBO0FBQ0E7QUFDQSxBQUNBO2tCQUFlLFlBQUE7eUJBQU0sY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEdBQUEsRUFBTixBQUFNO0FBQXJCIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkM6L1VzZXJzL1ZpbmNlIEtuaWdodC9jaS1yZWFjdCJ9
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+//used Next js to get setup -- final command npm run dev to get server started
+//in Next js anything placed within your pages folder will be a route within your application.  this folder must be named "pages".  All other folders may be name something else.
+//the routes are automatically built for you when you create the page names!  e.g. about.js will route to localhost:3000/about
+export default () => <div>Welcome to next.js!</div>
